4667a9f76e08843b7883b0a2edf41a2ebce9fd65
Henry Jiron <hjiron3@gatech.edu>
2024-08-31 13:44:25 -0400

    queriesfixedlength

---
+++ week5/cuberoot/Solution.java
@@ -0,0 +1,9 @@
+package week5.cuberoot;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(Math.cbrt(9));
+        System.out.println(Math.round(Math.pow(9, 1.0 / 3)));
+    }
+}
